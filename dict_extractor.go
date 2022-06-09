d5c95e4689b0b5ca119cdf5632f149b340fedbc7
James <james.sw.clark@gmail.com>
2022-06-09 17:37:59 +0100

    Put configurable variables together, so it's easier to generate more translation configs for other language pairs

--- dict_extractor.go
+++ dict_extractor.go
@@ -9,10 +9,15 @@ import (
 
 // thanks to https://stackoverflow.com/a/18479916/4261132 for read/write guidance
 func main() {
+	// ---------------- CONFIG START ----------------
 	// https://github.com/pquentin/wiktionary-translations
 	filePath := "frwiktionary-20140612-euradicfmt.txt"
 	l1 := "en"
 	l2 := "fr"
+	l1Index := 3
+	l2Index := 0
+	delimiter := ";"
+	// ---------------- CONFIG END ----------------
 
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -25,9 +30,9 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		parts := strings.Split(line, ";")
-		l1Word := parts[3] // en
-		l2Word := parts[0] // fr
+		parts := strings.Split(line, delimiter)
+		l1Word := parts[l1Index]
+		l2Word := parts[l2Index]
 		configLines = append(configLines, pairToEspanso(l1Word, l2Word, l2))
 		configLines = append(configLines, pairToEspanso(l2Word, l1Word, l1))
 	}
